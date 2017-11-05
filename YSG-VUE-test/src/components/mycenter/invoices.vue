1735cb3d89fdae6ae45460468824754dc8fe8ca6
jeff <jeff@gmail.com>
2017-11-06 10:41:18 +1300

    ysg first commit

---
+++ YSG-VUE-test/src/components/mycenter/invoices.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="invoice">
+        <div class="nav_mark"></div>
+        <yd-navbar title="INVOICES" fixed>
+            <router-link to="/home" slot="left">
+                <span class="back" ></span>
+            </router-link>
+        </yd-navbar>
+        <div class="invoice-content">
+            <!-- <div @click="showHeader"><button type="button">发票抬头</button></div> -->
+            <div @click="showHeader"><button type="button">发票抬头</button></div>
+            <div @click="showManager"><button type="button">发票管理</button></div>
+        </div>
+    </div>
+</template>
+<style>
+</style>
+
+<script>
+	import { mapGetters } from 'vuex'
+	import { mapState } from 'vuex'
+	export default {
+		data() {
+			return {
+                tokenSrc:''
+			};
+		},
+		created:function () {
+                let _this = this
+                // 获取token
+                var dataStr = JSON.stringify({"source":"YSG","otId":"1947364"});
+                dataStr = 'dataMap=' + dataStr;
+                $.ajax({
+                    type:'post',
+                    url:'https://bird.ioliu.cn/v1/?url=http://www.e-fp.cn/issp/ApiService?method=xforceplus.sys.qToken',
+                    async:false,
+                    data:dataStr,
+                    dataType:'jsonp',
+                    success:function(json){
+                        console.log('data—>',json.token);
+                        _this.tokenSrc = json.token;
+                        // //获取发票抬头
+                        // if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+                        //     openFile(_this.tokenSrc)
+                        // }else{
+                        //     openPdf(_this.tokenSrc)
+                        // }
+
+                    },
+                    error:function () {
+                    }
+                });
+		},
+        mounted:function () {
+            //一级页面falg
+            isHomePage(0)
+        },
+        methods: {
+            showHeader(){
+                this.$router.push({path:'/invoicesHeader',query:{token:this.tokenSrc}})
+            },
+            showManager(){
+                this.$router.push({path:'/invoicesManage',query:{token:this.tokenSrc}})
+            },
+
+        }
+	};
+</script>
---
+++ YSG-VUE/src/components/function/research.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="research">
+        <div class="nav_mark"></div>
+        <yd-navbar title="Research" fixed>
+           <router-link to="/home" slot="left">
+                <span class="back" ></span>
+            </router-link>
+        </yd-navbar>
+        <div class="webview">
+            <!-- <iframe class="iframe-style" :src="serviceUrl" frameborder="0" scrolling="yes"></iframe> -->
+        </div>
+    </div>
+</template>
+<style>
+    .iframe-style{width: 100%;height: 100%;padding-top: 1.5rem;}
+</style>
+
+<script>
+	import { mapGetters } from 'vuex'
+	import { mapState } from 'vuex'
+	export default {
+		data() {
+			return {
+				serviceUrl:'',
+			};
+		},
+		created:function () {
+            let _this = this;
+            let params;
+            if(localStorage.TOKEN){
+                params = {'token':localStorage.TOKEN,'hotelid':localStorage.HOTELID,lang:localStorage.LANGUAGE};
+            } else {
+                params = {'hotelid':localStorage.HOTELID,lang:localStorage.LANGUAGE};
+            }
+
+            this.$store.dispatch('getResearchUrl', params).then(function (res) {
+                _this.serviceUrl = res.data.data.serviceUrl;
+            });
+
+		},
+        mounted:function () {
+            //一级页面falg
+            isHomePage(0)
+        },
+	};
+</script>
