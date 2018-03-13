5c37f17a4d4d0b2f0aaaa77970857329a603816d
Daniel <1762169697@qq.com>
2018-03-14 09:03:55 +1300

    Daniel
    修改本地攻略滚动条问题

--- YSG-VUE/src/components/localRaiders/raiders.vue
+++ YSG-VUE/src/components/localRaiders/raiders.vue
@@ -1,195 +1,264 @@
 <template>
-  <div class="promotiom">
-      <section class="promotiom-box">
-          <h1>{{language.raiders.title}}</h1>
-      </section>
-      <section class="promotiom_index raiders">
-          <div class="swiper-container">
-              <div class="swiper-wrapper ra">
-                  <div class="swiper-slide" v-for="(item, index) in typeList" :class="{active:(index == 0)}" @click="changeTab(item.id)">
-                      <a><span>{{item.title}}</span></a> 
-                  </div>
-              </div>
-          </div>
-      </section>
-      <section class="promotiom_list s-list2">
-          <scroller  >
-              <ul>
-                  <li v-for="item in dataList" @click="toDetail(item.id)">
-                      <div :id="item.id">
-                          <h4>{{item.name}}</h4>
-                          <img :src='item.pic'/>
-                      </div>
-                      <div class="ss-location">
-                          <div class="ss-left">
-                              <p>{{item.address}}</p>
-                          </div>
-                          <div class="ss-left">
-                              <i></i> {{item.distance}} km
-                          </div>
-                      </div>
-                  </li>
-              </ul>
-              <p class="no_data" v-show="noData">{{language.common.noMoreData}}</p> 
-          </scroller>
-      </section>
-      <v-foot></v-foot>
-  </div>
+    <div><!--main div start-->
+        <div class="promotiom" id="section1"><!--section1 div start-->
+            <section class="promotiom-box">
+                <h1>{{language.raiders.title}}</h1>
+            </section>
+            <section class="promotiom_index raiders">
+                <div class="swiper-container">
+                    <div class="swiper-wrapper ra">
+                        <div class="swiper-slide" v-for="(item, index) in typeList" :class="{active:(index == 0)}" @click="changeTab(item.id)">
+                            <a><span>{{item.title}}</span></a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="promotiom_list s-list2">
+                <section class="g-scrollview">
+                    <ul>
+                        <li v-for="item in dataList" @click="toDetail(item.id)">
+                            <div :id="item.id">
+                                <h4>{{item.name}}</h4>
+                                <img :src='item.pic'/>
+                            </div>
+                            <div class="ss-location">
+                                <div class="ss-left">
+                                    <p>{{item.address}}</p>
+                                </div>
+                                <div class="ss-left">
+                                    <i></i> {{item.distance}} km
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                    <p class="no_data" v-show="noData">{{language.common.noMoreData}}</p>
+                </section>
+            </section>
+            <v-foot></v-foot>
+        </div><!--section1 div end-->
+
+        <div class="property" id="section2" style="display: none;">
+            <div class="nav_mark"></div>
+            <yd-navbar :title="data.name" fixed>
+                <span class="back" slot="left" @click="detailClose()"></span>
+            </yd-navbar>
+            <section class="resolve-box" v-html="content"></section>
+            <div class="location-icon" @click="openMap"></div>
+            <ul class="pdf-video" :class="{ 'one': isOne, 'two': isTwo}">
+                <li @click="toPDF" v-show="pdfFlag"></li>
+                <li @click="toVideo" v-show="videoFlag"></li>
+            </ul>
+        </div>
+
+    </div><!--main div end-->
 </template>
 <style>
-
+    .location-icon{position: fixed;width: 1rem;height: 1rem;bottom: 2.5rem;right:0;background: url("../../assets/images/icon-location.png")center no-repeat;background-size: .9rem;}
+    .judge{display: block;width: .5rem;height: .5rem;background: url("../../assets/images/icon_tips.png")center no-repeat;background-size: .5rem;}
 </style>
 
 <script>
-import { mapState } from "vuex";
-import { mapGetters } from "vuex";
-import foot from "../foot.vue";
-import { PopupPicker, XButton } from "vux";
-import { getDistance } from "../../config/mUtils";
+    import { mapState } from "vuex";
+    import { mapGetters } from "vuex";
+    import foot from "../foot.vue";
+    import { PopupPicker, XButton } from "vux";
+    import { getDistance } from "../../config/mUtils";
 
 
-export default {
-  data() {
-    return {
-      isItemShow: false,
-      typeList: [],
-      dataList: [],
-      nextPage: 0,
-      distanceFlag: false,
-      typeId: 1,
-      noData: false,
-      tabNum:1
+    export default {
+        data() {
+            return {
+                isItemShow: false,
+                typeList: [],
+                dataList: [],
+                nextPage: 0,
+                distanceFlag: false,
+                typeId: 1,
+                noData: false,
+                tabNum:1,
+                data:{},
+                srcUrl:'',
+                pdfShow:false,
+                videoShow:false,
+                isOne:false,
+                isTwo:false,
+                pdfFlag:false,
+                videoFlag:false,
+                btnFlag:false,
+                content:''
+            };
+        },
+        created: function() {
+            localStorage.footFlag = 3;
+        },
+        mounted: function() {
+            let _this = this;
+            //获取tab 列表
+            let params = {
+                hotelid: localStorage.HOTELID,
+                lang: localStorage.LANGUAGE
+            };
+            this.$store.dispatch("getPoiTypeList", params).then(function(res) {
+                _this.typeList = res.data.data.list;
+                _this.typeId = _this.typeList[0].id;
+                $(document).ready(function() {
+                    var mySwiper =new Swiper(".raiders .swiper-container", {
+                        pagination: ".swiper-pagination",
+                        slidesPerView: 3,
+                        paginationClickable: true,
+                        spaceBetween: 0,
+                        initialSlide :0
+                    });
+
+                    $(".ra").on("click", ".swiper-slide", function() {
+                        $(this)
+                            .addClass("active")
+                            .siblings()
+                            .removeClass("active");
+                    });
+
+                    //添加数据
+                    if (_this.nextPage != "-1") {
+                        var flag = "inifinte";
+                        var id = _this.typeId;
+                        _this.getList(id, flag);
+                    } else {
+                        done(false);
+                        _this.noData = true;
+                    }
+                });
+            });
+
+            //一级页面falg
+            isHomePage(1);
+        },
+        methods: {
+            detailClose:function(){
+                $("#section1").show();
+                $("#section2").hide();
+            },
+            toPDF:function () {
+                if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+                    openFile(this.data.pdf)
+                }else{
+                    openPdf(this.data.pdf)
+                }
+            },
+            toVideo: function () {
+                if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+                    openFile(this.data.video)
+                }else{
+                    openVideo(this.data.video)
+                }
+            },
+            openMap: function () {
+                openMap(this.data.lng,this.data.lat)
+            },
+            detailInit:function(){
+                this.$store.dispatch('showLoading');
+                if(this.data.pdf && this.data.video){
+                    this.btnFlag = true
+                    this.pdfFlag = true
+                    this.videoFlag = true
+                    this.isTwo = true
+                }else if(this.data.pdf){
+                    this.btnFlag = true
+                    this.pdfFlag = true
+                    this.isOne = true
+                }else if(this.data.video){
+                    this.btnFlag = true
+                    this.videoFlag = true
+                    this.isOne = true
+                }
+
+                var ssrBase = 'https://bird.ioliu.cn/v1/?url='
+                let _this = this;
+                if (this.data.detail) {
+                    $.get(ssrBase+this.data.detail,function (res) {
+                        _this.content = res;
+                        _this.$store.dispatch('hideLoading')
+                    })
+                } else {
+                    this.$store.dispatch('hideLoading')
+                }
+            },
+            getList: function(id, flag) {
+                let _this = this;
+                _this.typeId = id;
+                if (flag == "click") {
+                    this.dataList = [];
+                    _this.nextPage = 0;
+                    _this.noData = false;
+                }
+                setTimeout(function() {
+                    let params = {
+                        hotelid: localStorage.HOTELID,
+                        typeid: id,
+                        lang: localStorage.LANGUAGE,
+                        limit: 30,
+                        page: _this.nextPage
+                    };
+                    _this.$store.dispatch("getPoiList", params).then(res => {
+                        let list = res.data.data.list;
+                        _this.nextPage = res.data.data.nextPage;
+                        //获取距离,重组数据
+                        var dataArr = [];
+                        for (var item in list) {
+                            let distance = parseFloat(
+                                getDistance(
+                                    localStorage.lat,
+                                    localStorage.lng,
+                                    list[item].lat,
+                                    list[item].lng
+                                )
+                            ).toFixed(2);
+                            list[item].distance = distance;
+                            dataArr.push(list[item]);
+                        }
+                        _this.dataList = _this.dataList.concat(dataArr);
+                    });
+                }, 300);
+            },
+            infinite:function (done) {
+                let _this = this
+                done(true);
+                //添加数据
+                if(this.nextPage != '-1'){
+                    var flag = 'inifinte'
+                    var id =this.typeId
+                    this.getList(id, flag)
+                }else{
+                    done(false);
+                    this.noData = true;
+                }
+            },
+            changeTab: function(id) {
+                this.tabNum = id;
+                var flag = "click";
+                this.getList(id, flag);
+            },
+            toDetail: function(id) {
+                let data = {};
+                for (var key in this.dataList) {
+                    if (this.dataList[key].id == id) {
+                        data = this.dataList[key];
+                        break;
+                    }
+                }
+                $("#section1").hide();
+                $("#section2").show();
+                this.data = data;
+                this.detailInit();
+            }
+        },
+        components: {
+            "v-foot": foot,
+            PopupPicker
+        },
+        computed: {
+            ...mapState({
+                language: state => state.language.language
+            })
+        }
     };
-  },
-  created: function() {
-    localStorage.footFlag = 3;
-  },
-  mounted: function() {
-    let _this = this;
-    //获取tab 列表
-    let params = {
-      hotelid: localStorage.HOTELID,
-      lang: localStorage.LANGUAGE
-    };
-    this.$store.dispatch("getPoiTypeList", params).then(function(res) {
-      _this.typeList = res.data.data.list;
-      _this.typeId = _this.typeList[0].id;
-      $(document).ready(function() {
-        var mySwiper =new Swiper(".raiders .swiper-container", {
-              pagination: ".swiper-pagination",
-              slidesPerView: 3,
-              paginationClickable: true,
-              spaceBetween: 0,
-              initialSlide :(Number(translateScrollY.raidersTab)-1)
-        });
-
-        $(".ra").on("click", ".swiper-slide", function() {
-          $(this)
-            .addClass("active")
-            .siblings()
-            .removeClass("active");
-        });
-
-        //添加数据
-        if (_this.nextPage != "-1") {
-          var flag = "inifinte";
-          var id = _this.typeId;
-          _this.getList(id, flag);
-        } else {
-          done(false);
-          _this.noData = true;
-        }
-      });
-    });
-
-    //一级页面falg
-    isHomePage(1);
-  },
-  methods: {
-    getList: function(id, flag) {
-      let _this = this;
-      _this.typeId = id;
-      if (flag == "click") {
-        this.dataList = [];
-        _this.nextPage = 0;
-        _this.noData = false;
-      }
-      setTimeout(function() {
-        let params = {
-          hotelid: localStorage.HOTELID,
-          typeid: id,
-          lang: localStorage.LANGUAGE,
-          limit: 30,
-          page: _this.nextPage
-        };
-        _this.$store.dispatch("getPoiList", params).then(res => {
-          let list = res.data.data.list;
-          _this.nextPage = res.data.data.nextPage;
-          //获取距离,重组数据
-          var dataArr = [];
-          for (var item in list) {
-            let distance = parseFloat(
-              getDistance(
-                localStorage.lat,
-                localStorage.lng,
-                list[item].lat,
-                list[item].lng
-              )
-            ).toFixed(2);
-            list[item].distance = distance;
-            dataArr.push(list[item]);
-          }
-          _this.dataList = _this.dataList.concat(dataArr);
-          console.log(_this.dataList);
-        });
-        //设置tab标签默认选项
-        let a=Number(translateScrollY.raidersTab);
-        let b=Number(_this.tabNum);
-        if(a!=b){
-          setTimeout(function(){
-              $(".swiper-wrapper .swiper-slide:eq("+(a-1)+")").click();
-          },700);
-        }
-      }, 300);
-    },
-    infinite:function (done) {
-        let _this = this
-        done(true);
-        //添加数据
-        if(this.nextPage != '-1'){
-            var flag = 'inifinte'
-            var id =this.typeId
-            this.getList(id, flag)
-        }else{
-            done(false);
-            this.noData = true;
-        }
-    },
-    changeTab: function(id) {
-      this.tabNum = id;
-      translateScrollY.raidersTab = id;
-      var flag = "click";
-      this.getList(id, flag);
-    },
-    toDetail: function(id) {
-      let data = {};
-      for (var key in this.dataList) {
-        if (this.dataList[key].id == id) {
-          data = this.dataList[key];
-          break;
-        }
-      }
-      this.$router.push({ path: "/raidersDetail", query: { data: data } });
-    }
-  },
-  components: {
-    "v-foot": foot,
-    PopupPicker
-  },
-  computed: {
-    ...mapState({
-      language: state => state.language.language
-    })
-  }
-};
 </script>
