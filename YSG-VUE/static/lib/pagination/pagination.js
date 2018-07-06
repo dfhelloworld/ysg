a5b80e3569034ea34fd1f16324129bab434014a7
Daniel <duanlanling@gmail.com>
2018-07-06 15:24:55 +1200

    添加分页信息
    添加分页信息

--- YSG-VUE/static/lib/pagination/pagination.js
+++ YSG-VUE/static/lib/pagination/pagination.js
@@ -16,70 +16,39 @@ window.Pagination = function(target, options) {
     this.pageCallBack = this.options.pageCallBack || function(){};//初始化回调函数
     //初始化分页内容
     if(this.pages>1){
-        let pageContent = '<a href="#" class="pagerCurrent">'+this.page+'</a>';
-      //   for(let i =0;i<this.pages;i++){
-      //       if(i==3){
-      //           pageContent = pageContent +'<span class="pagerNone">……</span>';
-      //       }
-      //       if((i+1)==this.page){
-      //           pageContent = pageContent +'<a href="#" class="pagerCurrent">'+(i+1)+'</a>';
-      //       }else{
-      //           pageContent = pageContent +'<a href="#" class="pagerBtnNum">'+(i+1)+'</a>';
-      //       }
-      //   }
-        let prevPageNode = '上一页';
-        let nextPageNode = '下一页';
+        let pageContent = '';
+        let prevPageNode = '第';
+        let nextPageNode = '页';
         if(this.lang!='zh'){
-            prevPageNode = 'Prev';
-            nextPageNode = 'Next';
+            prevPageNode = 'Page';
+            nextPageNode = '';
         }
-        let targetHtml=`
-                        <div class="pager">
-                            <a href="#" class="pagerBtn">`+prevPageNode+`</a>
+        let contentHtml = '';
+        for(let i=0;i<this.pages;i++){
+            contentHtml = contentHtml + 
                             `
-                            +pageContent+
-                            `
-                            <a href="#" class="pagerBtn">`+nextPageNode+`</a>
-                        </div>
-                    `;
+                                <option value="`+(i+1)+`">`+prevPageNode+(i+1)+nextPageNode+`</option>
+                            `;
+        }
+        let targetHtml=`<div style="width:100%;">
+                            <center>
+                                <div class="pagination">
+                                    <select>
+                                    `
+                                    +contentHtml+
+                                    `
+                                    </select>
+                                </div>
+                            </center>
+                        </div>`;
         $(this.target).html(targetHtml);
         //添加点击事件
         let _this = this;
-        //上一页
-        $(this.target).find("a:eq(0)").click(function(){
-            if(_this.page!=1){
-                _this.page = _this.page -1;
-              //   let prePage = $(_this.target).find(".pagerCurrent").prev(".pagerBtnNum");
-              //   $(_this.target).find(".pagerCurrent").addClass("pagerBtnNum");
-              //   $(_this.target).find(".pagerCurrent").removeClass("pagerCurrent");
-              //   $(prePage).addClass("pagerCurrent");
-              //   $(prePage).removeClass("pagerBtnNum");
-                _this.pageCallBack(_this.page);
-                $(_this.target).find(".pagerCurrent").html(_this.page);
-            }
-        });
-        //下一页
-        $(this.target).find("a").last().click(function(){
-            if(_this.page!=_this.pages){
-                _this.page = _this.page +1;
-              //   let nextPage = $(_this.target).find(".pagerCurrent").next(".pagerBtnNum");
-              //   $(_this.target).find(".pagerCurrent").addClass("pagerBtnNum");
-              //   $(_this.target).find(".pagerCurrent").removeClass("pagerCurrent");
-              //   $(nextPage).addClass("pagerCurrent");
-              //   $(nextPage).removeClass("pagerBtnNum");
-                _this.pageCallBack(_this.page);
-                $(_this.target).find(".pagerCurrent").html(_this.page);
-            }
-        });
         //选择页数
-      //   $(this.target).find("a[class!=pagerBtn]").click(function(){
-      //       _this.page = Number($(this).html());
-      //       $(_this.target).find(".pagerCurrent").addClass("pagerBtnNum");
-      //       $(_this.target).find(".pagerCurrent").removeClass("pagerCurrent");
-      //       $(this).addClass("pagerCurrent");
-      //       $(this).removeClass("pagerBtnNum");
-      //       _this.pageCallBack(_this.page);
-      //   });
+        $(this.target).find("select").change(function(){
+            _this.page = Number($(this).val());
+            _this.pageCallBack(_this.page);
+        });
     }
   };
   //设置分页方法
