cc2a562fe2da927d6b66f873619eed015eb9a801
Daniel <duanlanling@gmail.com>
2018-09-17 14:34:16 +1200

    增加提示信息
    增加提示信息

--- YSG-VUE/src/components/function/laundry.vue
+++ YSG-VUE/src/components/function/laundry.vue
@@ -27,9 +27,11 @@
                 <td width="5%">&nbsp;</td>
                 <td>
                     <div v-if="isZH">
+                       <div style="font-size:14px;font-weight:bold;">提示</br></br></div>
                        机器人将到你的房间收取洗衣，请将要洗的衣物放入洗衣袋，并与洗衣单一起放入服务机器人。机器人将在3分钟内到您房间门口收取洗衣，请注意接听房间电话。
                     </div>
                     <div v-if="!isZH">
+                       <div style="font-size:14px;font-weight:bold;">Reminder</br></br></div>
                        The  Robot “ Xiao Ya”  will come to your room to collect the laundry. Please put your laundry ,laundry list into the laundry bag  and put them into the service robot. The service Robot  will be at the door of your apartment within 3 minutes. Please pay attention to answer the in-room phone.
                     </div>
                 </td>
